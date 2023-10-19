0861cae089438cf84263cb7af07b89ba25ed785b
mo-adel-97 <113055631+mo-adel-97@users.noreply.github.com>
2023-10-20 01:12:02 +0300

    test
    test by add fetch function

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import './App.css';
+import { useEffect,useState } from 'react';
 
 function App() {
+  const [data,setData] = useState('')
+
+  useEffect(() => {
+    fetch("https://api-profile-git-main-mohameds-projects-38a34405.vercel.app/data")
+    .then(response => response.json())
+        // 4. Setting *dogImage* to the image url that we received from the response above
+    .then(data => setData(data.message))
+  },[])
   return (
     <div className="App">
        <h1>
-        hello from vercel 
+        hello ${data} from vercel 
        </h1>
     </div>
   );
